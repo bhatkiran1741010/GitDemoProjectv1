f168048be87845b04e8523a83ab7f28542c0c5c4
kiran <kiran@DESKTOP-KK6FE83>
2022-07-17 01:47:33 +0530

    third commit

--- src/com/kiran/Test2.java
+++ src/com/kiran/Test2.java
@@ -5,6 +5,10 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("from bhatkiran74.................");
+System.out.println("from bhatkiran74.................");
+System.out.println("from bhatkiran74.................");
+System.out.println("from bhatkiran74.................");
+System.out.println("from bhatkiran74.................");
 	}
 
 }

844b77f99c1576c3d0fdc03e890357e90ef22420
kiran <kiran@DESKTOP-KK6FE83>
2022-07-17 00:51:48 +0530

    first commit

---
+++ src/com/kiran/Test1.java
@@ -0,0 +1,18 @@
+package com.kiran;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String str = "yes";
+
+		if (str == "yes") {
+			System.out.println("Eat something");
+
+		} else {
+
+			System.out.println("Do work....");
+		}
+
+	}
+}
